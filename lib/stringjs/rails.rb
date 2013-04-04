4368fa29b2b28d8ece850e204112460a38e7bab0
Jesper Josefsson <jesper.josefsson@gmail.com>
2013-04-04 14:38:01 +0200

    Initial commit

---
+++ lib/stringjs/rails.rb
@@ -0,0 +1,7 @@
+require "stringjs/rails/version"
+
+module Stringjs
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/stringjs/rails/version.rb
@@ -0,0 +1,5 @@
+module Stringjs
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
